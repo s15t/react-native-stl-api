3638d64ee44d91822dc7a84a1b84bf322d3bcd8b
Seongwoo Park <lucian@5corpion.dev>
2022-09-15 16:53:09 +0900

    feat(android): request permissions and setColorMode

--- android/src/main/java/kr/co/smartsignal/api/StlApiModule.java
+++ android/src/main/java/kr/co/smartsignal/api/StlApiModule.java
@@ -1,17 +1,25 @@
 package kr.co.smartsignal.api;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.net.Uri;
+import android.os.Build;
+import android.os.PowerManager;
+import android.provider.Settings;
 import android.util.Base64;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.BaseActivityEventListener;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -26,12 +34,44 @@ import java.util.Map;
 
 @ReactModule(name = StlApiModule.NAME)
 public class StlApiModule extends ReactContextBaseJavaModule {
+  private static final int OVERLAY_PERMISSION_REQUEST = 6000;
+  private static final int IGNORE_BATTERY_OPT_PERMISSION_REQUEST = 6001;
+  private static final String E_ACTIVITY_DOES_NOT_EXIST = "E_ACTIVITY_DOES_NOT_EXIST";
+  private static final String E_FAILED_TO_PERMIT_OVERLAY = "E_FAILED_TO_PERMIT_OVERLAY";
+  private static final String E_FAILED_TO_PERMIT_BATTERY_OPT = "E_FAILED_TO_PERMIT_BATTERY_OPT";
+
   public static final String NAME = "common";
   private final ReactApplicationContext reactContext;
+
+  private Promise mOverlayPromise;
+  private Promise mIgnoreBatteryOptPromise;
 
   public StlApiModule(ReactApplicationContext context) {
     super(context);
     reactContext = context;
+    reactContext.addActivityEventListener(new BaseActivityEventListener() {
+      @Override
+      public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+          case OVERLAY_PERMISSION_REQUEST:
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+              mOverlayPromise.resolve(canDrawOverlays());
+            } else {
+              mOverlayPromise.resolve(true);
+            }
+            break;
+          case IGNORE_BATTERY_OPT_PERMISSION_REQUEST:
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+              mIgnoreBatteryOptPromise.resolve(isIgnoringBatteryOptimizations());
+            } else {
+              mIgnoreBatteryOptPromise.resolve(true);
+            }
+            break;
+          default:
+            break;
+        }
+      }
+    });
   }
 
   @Override
@@ -60,8 +100,122 @@ public class StlApiModule extends ReactContextBaseJavaModule {
       constants.put("version", "");
       constants.put("buildVersion", null);
       constants.put("identifier", "");
+    } finally {
+      constants.put("COLOR_MODE", getColorModeConstants());
     }
     return constants;
+  }
+
+  private Map<String, Object> getColorModeConstants() {
+    final Map<String, Object> constants = new HashMap<>();
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      constants.put("DEFAULT", ActivityInfo.COLOR_MODE_DEFAULT);
+      constants.put("WIDE_COLOR_GAMUT", ActivityInfo.COLOR_MODE_WIDE_COLOR_GAMUT);
+      constants.put("HDR", ActivityInfo.COLOR_MODE_HDR);
+    } else {
+      constants.put("DEFAULT", 0);
+      constants.put("WIDE_COLOR_GAMUT", 1);
+      constants.put("HDR", 2);
+    }
+    return constants;
+  }
+
+  @ReactMethod
+  public void setWideColorGamut(int colorMode, Promise promise) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      Activity activity = getCurrentActivity();
+      if (activity != null) {
+        activity.getWindow().setColorMode(colorMode);
+        promise.resolve(null);
+      } else {
+        promise.reject(E_ACTIVITY_DOES_NOT_EXIST, "Activity doesn't exist");
+      }
+    }
+  }
+
+  @ReactMethod
+  public boolean canDrawOverlays() {
+    boolean canDraw;
+    Context context = reactContext.getApplicationContext();
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      canDraw = Settings.canDrawOverlays(context);
+    } else {
+      canDraw = false;
+    }
+    return canDraw;
+  }
+
+  @ReactMethod
+  public void reqOverlayPermissions(Promise promise) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+      promise.resolve(true);
+    } else {
+      Context context = reactContext.getApplicationContext();
+
+      Activity activity = getCurrentActivity();
+      if (activity == null) {
+        promise.reject(E_ACTIVITY_DOES_NOT_EXIST, "Activity doesn't exist");
+        return;
+      }
+
+      mOverlayPromise = promise;
+      try {
+        Intent intent;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+          intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+        } else {
+          intent = new Intent(
+            Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+            Uri.parse("package:" + context.getPackageName())
+          );
+        }
+        activity.startActivityForResult(intent, OVERLAY_PERMISSION_REQUEST);
+      } catch (Exception e) {
+        mOverlayPromise.reject(E_FAILED_TO_PERMIT_OVERLAY, e);
+        mOverlayPromise = null;
+      }
+    }
+  }
+
+  @ReactMethod
+  public boolean isIgnoringBatteryOptimizations() {
+    boolean isIgnored = false;
+    Context context = reactContext.getApplicationContext();
+
+    PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      isIgnored = pm.isIgnoringBatteryOptimizations(context.getPackageName());
+    }
+    return isIgnored;
+  }
+
+  @ReactMethod
+  @SuppressLint("BatteryLife")
+  public void reqIgnoringBatteryOptimizations(Promise promise) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+      promise.resolve(true);
+    } else {
+      Context context = reactContext.getApplicationContext();
+
+      Activity activity = getCurrentActivity();
+      if (activity == null) {
+        promise.reject(E_ACTIVITY_DOES_NOT_EXIST, "Activity doesn't exist");
+        return;
+      }
+
+      mIgnoreBatteryOptPromise = promise;
+      try {
+        Intent intent = new Intent(
+          Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
+          Uri.parse("package:" + context.getPackageName())
+        );
+        activity.startActivityForResult(intent, IGNORE_BATTERY_OPT_PERMISSION_REQUEST);
+      } catch (Exception e) {
+        mIgnoreBatteryOptPromise.reject(E_FAILED_TO_PERMIT_BATTERY_OPT, e);
+        mIgnoreBatteryOptPromise = null;
+      }
+    }
   }
 
   @ReactMethod
--- src/index.tsx
+++ src/index.tsx
@@ -39,12 +39,51 @@ const API = {
     version: common.version as string,
     buildVersion: common.buildVersion as number,
     identifier: common.identifier as string,
+    setWideColorGamut: function (colorMode: number): Promise<null> {
+      if (Platform.OS === 'android') {
+        return common.setWideColorGamut(colorMode);
+      }
+      return Promise.resolve(null);
+    },
     getKeyHashes: function (): Promise<string[]> {
       if (Platform.OS !== 'android') {
         return Promise.resolve([]);
       }
       return common.getKeyHashes();
     },
+    canDrawOverlays: function (): boolean {
+      if (Platform.OS === 'android') {
+        return common.canDrawOverlays();
+      } else {
+        return true;
+      }
+    },
+    reqOverlayPermissions: function (): Promise<boolean> {
+      if (Platform.OS === 'android') {
+        return common.reqOverlayPermissions();
+      } else {
+        return Promise.resolve(true);
+      }
+    },
+    isIgnoringBatteryOptimizations: function (): boolean {
+      if (Platform.OS === 'android') {
+        return common.isIgnoringBatteryOptimizations();
+      } else {
+        return true;
+      }
+    },
+    reqIgnoringBatteryOptimizations: function (): Promise<boolean> {
+      if (Platform.OS === 'android') {
+        return common.reqIgnoringBatteryOptimizations();
+      } else {
+        return Promise.resolve(true);
+      }
+    },
+  },
+  COLOR_MODE: {
+    DEFAULT: common.COLOR_MODE.DEFAULT as number,
+    WIDE_COLOR_GAMUT: common.COLOR_MODE.WIDE_COLOR_GAMUT as number,
+    HDR: common.COLOR_MODE.HDR as number,
   },
   register: {
     getNearbyDevice: function (): Promise<RegisterGetNearbyDevice> {
