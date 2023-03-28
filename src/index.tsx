3349d97fef8fc7dd060892c49e503e9743725454
Seongwoo Park <lucian@5corpion.dev>
2023-03-28 13:24:43 +0900

    fix: allocate type

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
-// @ts-expect-error
-import CommonModule from './modules/Common';
-// @ts-expect-error
-import BluetoothModule from './modules/Bluetooth';
+import { Platform } from 'react-native';
+import type BluetoothAndroid from './modules/Bluetooth.android';
+import type BluetoothIOS from './modules/Bluetooth.ios';
+import type CommonAndroid from './modules/Common.android';
+import type CommonIOS from './modules/Common.ios';
 
-const API = {
-  ble: BluetoothModule,
-  common: CommonModule,
+interface StlApi {
+  ble: typeof BluetoothAndroid | typeof BluetoothIOS;
+  common: typeof CommonAndroid | typeof CommonIOS;
+  register: {
+    getNearbyDevice: () => void;
+  };
+}
+
+const StlApi: StlApi = {
+  ble: Platform.select({
+    android: require('./modules/Bluetooth.android'),
+    ios: require('./modules/Bluetooth.ios'),
+  }),
+  common: Platform.select({
+    android: require('./modules/Common.android'),
+    ios: require('./modules/Common.ios'),
+  }),
   register: {
     getNearbyDevice: function (): void {},
   },
 };
 
-export default API;
+export default StlApi;
